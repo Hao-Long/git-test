59ffc087179b0fe8a5d0e2409b44ddff93448b23
LiuHL <1620347568@qq.com>
2022-05-06 14:26:23 +0800

    commit third

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,6 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git");
-        System.out.println("Hello git");System.out.println("Hello git");
+        System.out.println("Hello git3");System.out.println("Hello git");
     }
 }
